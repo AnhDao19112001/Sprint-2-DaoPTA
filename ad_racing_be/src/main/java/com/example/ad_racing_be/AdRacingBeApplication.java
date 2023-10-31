cac3320649aadf535c6681acfa3aa54374ed7746
AnhDao19112001 <phantaanhdao@gmail.com>
2023-10-31 13:35:42 +0700

    [day 2][Prototype Login]

---
+++ ad_racing_be/src/main/java/com/example/ad_racing_be/AdRacingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ad_racing_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdRacingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdRacingBeApplication.class, args);
+    }
+
+}
---
+++ ad_racing_be/src/test/java/com/example/ad_racing_be/AdRacingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ad_racing_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdRacingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
