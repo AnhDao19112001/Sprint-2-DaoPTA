d5982d3dec8baaeccc52c21fbd19cf34b7478972
AnhDao19112001 <phantaanhdao@gmail.com>
2023-11-03 15:18:52 +0700

    [AD RACING][Login Register]

--- ad_racing_fe/src/App.js
+++ ad_racing_fe/src/App.js
@@ -4,14 +4,16 @@ import Cart from "./component/Cart";
 import DetailProduct from "./component/DetailProduct";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Register from "./component/Register";
 
 function App() {
   return (
     // <Login />
+      <Register />
     //   <DetailProduct/>
     //   <Header />
     //   <Footer/>
-      <Cart />
+    //   <Cart />
   );
 }
 
