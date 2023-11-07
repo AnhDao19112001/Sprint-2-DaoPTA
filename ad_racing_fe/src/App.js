2d3c0a615e0d6b03ca3fd3846adcb7e75473d3c9
AnhDao19112001 <phantaanhdao@gmail.com>
2023-11-07 17:06:58 +0700

    [AD RACING][Author Authen]

--- ad_racing_fe/src/App.js
+++ ad_racing_fe/src/App.js
@@ -4,16 +4,33 @@ import Cart from "./component/Cart";
 import Register from "./component/user/Register";
 import {Route, Routes} from "react-router-dom";
 import DetailProduct from "./component/DetailProduct";
+import Authentication from "./component/user/Authentication";
+import {EnumAppUserRole} from "./component/user/EnumAppUserRoles";
+import AuthorOfCustomer from "./component/user/AuthorOfCustomer";
 
 function App() {
-  return (
-      <Routes>
-        <Route path={`/login`} element={<Login />} />
-        <Route path={`/register`} element={<Register />} />
-        <Route path={`/cart`} element={<Cart />} />
-        <Route path={`/detail`} element={<DetailProduct />} />
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path={`/login`} element={<Login/>}/>
+            <Route path={`/register`} element={<Register/>}/>
+            <Route path={`/cart`} element={<Cart/>}/>
+            <Route path={`/detail`} element={<DetailProduct/>}/>
+
+            <Route element={
+                <Authentication
+                    allowedRoles={[
+                        EnumAppUserRole.ROLE_ADMIN,
+                        EnumAppUserRole.ROLE_CUSTOMER,
+                    ]}
+                />
+            }>
+                <Route element={<AuthorOfCustomer />}>
+                    <Route path={`/detail`} element={<DetailProduct />} />
+                </Route>
+            </Route>
+        </Routes>
+
+    );
 }
 
 export default App;
